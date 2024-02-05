482e8e033b442a41439114ffc3a312adc219ae2d
nilesh <nielshvedpathak2322@gmail.com>
2024-02-05 14:20:58 +0530

    my first c program

--- variable.c
+++ variable.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+void main()
+// this is a program defining variables in c
+{
+ int a=30;
+ char b ='#';
+ float pi=3.14;
+ printf("\nthe value of a is %d",a);
+ printf("\nthe character b = %c",b);
+ printf("\nthe value of pi is~git  %f",pi);
+ 
+}
